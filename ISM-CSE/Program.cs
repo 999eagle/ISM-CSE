6b5c09d68f21f85572fb55ec31a334450aa2bb60
Erik Tauchert <999eagle@999eagle.moe>
2018-01-16 17:12:23 +0100

    Add project files.

---
+++ ISM-CSE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM_CSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
