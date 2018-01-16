f57532c6e5bbf4477c68b6e7393650b4b6e65ec6
Erik Tauchert <999eagle@999eagle.moe>
2018-01-16 17:15:05 +0100

    Use correct whitespace

--- ISM-CSE/Program.cs
+++ ISM-CSE/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 
 namespace ISM_CSE
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
