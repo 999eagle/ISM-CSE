c7ab6409e5b4b27cdec07e962309838911b8b3e7
Erik Tauchert <999eagle@999eagle.moe>
2018-01-18 20:05:32 +0100

    Add command line option to specfiy number of search results

--- ISM-CSE/Program.cs
+++ ISM-CSE/Program.cs
@@ -1,57 +1,82 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
 using Google.Apis.Services;
+using CommandLine;
 
 namespace ISM_CSE
 {
 	class Program
 	{
+		class Options
+		{
+			[Option('c', "count", Default = 10, HelpText = "Number of search results.", MetaValue = "number")]
+			public int Count { get; set; }
+			[Value(0, Required = true, HelpText = "Term to search for.", MetaName = "search term")]
+			public IEnumerable<string> SearchTerm { get; set; }
+		}
+
 		static void Main(string[] args)
 		{
 			Console.OutputEncoding = Encoding.UTF8;
-			var config = ReadConfig();
-			if (config == null) return;
-			if (args.Length == 0)
-			{
-				Console.WriteLine($"Usage: {Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0])} <search term>");
-				return;
-			}
-			Task.Run(async () =>
-			{
-				var searchService = new CustomsearchService(new BaseClientService.Initializer()
-				{
-					ApplicationName = "ISM-CSE",
-					ApiKey = config.APIKey
-				});
-				var request = searchService.Cse.List(String.Join(" ", args));
-				request.Cx = config.SearchEngineID;
-				request.Num = 10;
-				try
-				{
-					var response = await request.ExecuteAsync();
-					Console.WriteLine(
-						String.Join("",
-						Enumerable.Range(0, response.Items.Count)
-							.Select(i => (index: i, item: response.Items[i]))
-							.Aggregate("",
-								(text, t) => text + $"Search result {t.index + 1}: {t.item.Title} <{t.item.Link}>\n{t.item.Snippet}\n\n")
-							.Reverse()
-							.SkipWhile(c => c == '\n')
-							.Reverse()));
-				}
-				catch (Google.GoogleApiException ex)
-				{
-					Console.WriteLine($"API call failed. Reason: {ex.Message}");
-				}
-			}).Wait();
+			var parsedArgs = Parser.Default.ParseArguments<Options>(args);
+			parsedArgs
+				.WithParsed(options => AsyncMain(options).Wait());
 #if DEBUG
 			Console.ReadLine();
 #endif
+		}
+
+		static async Task AsyncMain(Options options)
+		{
+			var config = ReadConfig();
+			if (config == null) return;
+			var searchService = new CustomsearchService(new BaseClientService.Initializer()
+			{
+				ApplicationName = "ISM-CSE",
+				ApiKey = config.APIKey
+			});
+			try
+			{
+				var items = await RequestResults(options.Count);
+				Console.WriteLine(
+					String.Join("",
+					Enumerable.Range(0, items.Count())
+						.Zip(items, (idx, item) => (index: idx, item: item))
+						.Aggregate("",
+							(text, t) => text + $"Search result {t.index + 1}: {t.item.Title} <{t.item.Link}>\n{t.item.Snippet}\n\n")
+						.Reverse()
+						.SkipWhile(c => c == '\n')
+						.Reverse()));
+			}
+			catch (Google.GoogleApiException ex)
+			{
+				Console.WriteLine($"API call failed. Reason: {ex.Message}");
+			}
+
+			async Task<IEnumerable<Result>> RequestResults(int count, int start = 1)
+			{
+				const int maxNum = 10;
+				var request = searchService.Cse.List(String.Join(" ", options.SearchTerm));
+				request.Cx = config.SearchEngineID;
+				request.Num = Math.Min(count, maxNum);
+				request.Start = start;
+				var response = await request.ExecuteAsync();
+				if (count <= maxNum || response.Items.Count < response.Items.Count)
+				{
+					return response.Items;
+				}
+				else
+				{
+					return response.Items.Concat(await RequestResults(count - maxNum, start + response.Items.Count));
+				}
+			}
 		}
 
 		static Config ReadConfig()
